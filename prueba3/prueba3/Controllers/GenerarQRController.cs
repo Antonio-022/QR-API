82eac7b12f1ae4d3a4f1a120a3011bb22cd0472b
Antonio-022 <68829532+Antonio-022@users.noreply.github.com>
2022-05-30 08:11:29 -0400

    Add files via upload

---
+++ prueba3/prueba3/Controllers/GenerarQRController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prueba3.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prueba3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenerarQRController : ControllerBase
+    {
+        [HttpGet("QR")]
+        public ActionResult<byte[]> GetTareas(string dato)
+        {
+            return GenQR.GenerarQR(dato);
+        }
+    }
+}
---
+++ prueba3/prueba3/Tools/GenQR.cs
@@ -0,0 +1,26 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prueba3.Tools
+{
+    public class GenQR
+    {
+
+            public static byte[] GenerarQR(string pTexto)
+            {
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(pTexto, QRCodeGenerator.ECCLevel.Q);
+
+                QRCode qrCodePNG = new QRCode(qrCodeData);
+                Bitmap qrCodeImageBmp = qrCodePNG.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile("C:\\logo4.jpg"));
+                ImageConverter converter = new ImageConverter();
+                return (byte[])converter.ConvertTo(qrCodeImageBmp, typeof(byte[]));
+
+            }
+    }
+}
